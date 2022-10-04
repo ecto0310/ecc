fc564ed49ca163bfaa1cb79b304189222c09af66
ecto0310 <ecto0310@gmail.com>
2022-10-04 21:45:11 +0900

    [add] file open

---
+++ src/error.h
@@ -0,0 +1,6 @@
+#ifndef SRC_ERROR_H_
+#define SRC_ERROR_H_
+
+void error(char *fmt, ...);
+
+#endif  // SRC_ERROR_H_
--- src/main.c
+++ src/main.c
@@ -1,1 +1,9 @@
-int main(int argc, char *argv[]) { return 0; }
+#include "file.h"
+
+int main(int argc, char **argv) {
+  char *input_file = argv[1];
+
+  char *source = read_file(input_file);
+
+  return 0;
+}
